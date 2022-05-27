248244e42ac47f66c94cd53471e2cbf38ed5ea11
Mohammed Aabid A <mohammed@mavencart.com>
2022-05-27 19:27:29 +0530

    Initial Commit

---
+++ swift-rive/swift_riveApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_riveApp.swift
+//  swift-rive
+//
+//  Created by Mohammed aabid on 27/05/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_riveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
